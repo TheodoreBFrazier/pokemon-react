ed9611844db042b3782234a934193f1620960f14
Greg Testo <gregt@pursuit.org>
2021-11-11 16:48:18 -0500

    added select pokemon functionality

--- src/App.js
+++ src/App.js
@@ -9,25 +9,31 @@ class App extends Component{
     super();
     this.state = {
       pokemonList: data.pokemon,
-      selectedPokemon: {}
+      selectedPokemon: {name: "No Pokemon Selected", image:"https://via.placeholder.com/100"}
     }
   }
   
+  handlePokemonClick=(pokemon)=>{
+    this.setState({
+      selectedPokemon: pokemon
+    });
+  }
+
   render(){
 
     let pokemonNameElArr = this.state.pokemonList.map((pokemon)=>{
       return(
-        <PokemonCard pokemon={pokemon} />
+        <PokemonCard key={pokemon.id} pokemon={pokemon} handlePokemonClick={this.handlePokemonClick} />
       )
-    })
+    });
 
     return(
       <div className="app" id="app-container">
         <h1>Pokémon React Code Along</h1>
         <div>
           <h3>Currently Selected Pokemon</h3>
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="Selected pokemon image" />
-          <div>Pikachu</div>
+          <img src={this.state.selectedPokemon.image} alt="Selected pokemon image" />
+          <div>{this.state.selectedPokemon.name}</div>
         </div>
         <div id="pokemon-list-container">
           { pokemonNameElArr }
