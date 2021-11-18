68e4d9df81ec7238a56a793ac3a4dab282e23fba
Greg Testo <gregt@pursuit.org>
2021-11-18 14:19:21 -0500

    started team feature

--- src/App.js
+++ src/App.js
@@ -7,14 +7,22 @@ class App extends Component{
     super();
     this.state = {
       pokemonList: data.pokemon,
-      selectedPokemon: {name: "No Pokemon Selected", image:"https://via.placeholder.com/100"}
+      teamList: []
     }
+  }
+  handlePokemonClick = (pokemon)=> {
+    this.setState({
+      teamList: [...this.state.teamList, pokemon]
+    })
   }
   
   handlePokemonClick=(pokemon)=>{
+    
+    let newArr = [ ...this.state.teamList, pokemon ];
+    
     this.setState({
-      selectedPokemon: pokemon
-    });
+      teamList: newArr
+    })
   }
 
   render(){
@@ -22,7 +30,7 @@ class App extends Component{
     let pokemonNameElArr = this.state.pokemonList.map((pokemon)=>{
       let { image, name, weight, height, hp, atk, def } = pokemon;
       return(
-        <div 
+        <div
             className="pokemon-item-container"
             onClick={()=>this.handlePokemonClick(pokemon)}
         >
@@ -40,11 +48,6 @@ class App extends Component{
     return(
       <div className="app" id="app-container">
         <h1>Pokémon React Code Along</h1>
-        <div>
-          <h3>Currently Selected Pokemon</h3>
-          <img src={this.state.selectedPokemon.image} alt="Selected pokemon image" />
-          <div>{this.state.selectedPokemon.name}</div>
-        </div>
         <div id="pokemon-list-container">
           { pokemonNameElArr }
         </div>
